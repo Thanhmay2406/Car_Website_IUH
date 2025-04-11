29780c0ec66c750096cad527c14e390b52f8def5
Thanhmay2406 <Thanhmay2406@gmail.com>
2025-04-11 14:17:22 +0700

    update: homepage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,56 +5,90 @@ document.addEventListener("DOMContentLoaded", function () {
 
     function filterSubTabs(selectedCategory) {
         subTabButtons.forEach(btn => {
-            if (selectedCategory === "BMWM" && btn.dataset.type === "M") {
-                btn.classList.remove("un-active");
-                btn.classList.add("sub-active");
-            } else if (selectedCategory === "BMWi" && btn.dataset.type === "i") {
-                btn.classList.remove("un-active");
-                btn.classList.add("sub-active");
-            } else if (selectedCategory === "BMW") {
-                btn.classList.remove("un-active");
-                btn.classList.remove("sub-active");
-            } else {
+            if (btn.dataset.type === "M") {
                 btn.classList.add("un-active");
-                btn.classList.remove("sub-active");
+            }
+            else if (btn.dataset.type === "i") {
+                btn.classList.add("un-active");
+            }
+            else {
+                btn.classList.remove("un-active")
             }
         });
+        if (selectedCategory === "BMWM") {
+            subTabButtons.forEach(btn => {
+                if (btn.dataset.type === "M") {
+                    btn.classList.remove("un-active");
+                    btn.classList.add("sub-active")
+                }
+                else {
+                    btn.classList.add("un-active")
+                }
+            });
+        }
+        else if (selectedCategory === "BMWi") {
+            subTabButtons.forEach(btn => {
+                if (btn.dataset.type === "i") {
+                    btn.classList.remove("un-active");
+                    btn.classList.add("sub-active")
+                }
+                else {
+                    btn.classList.add("un-active")
+                }
+            });
+        }
     }
-
     function filterCars() {
         const activeCategory = document.querySelector(".tab-btn.active").dataset.category;
-        const activeType = document.querySelector(".sub-tab-btn.sub-active")?.dataset.type;
+        const activeType = document.querySelector(".sub-tab-btn.sub-active").dataset.type;
+        const subTabButtonM = document.querySelectorAll(".tab-btn[M]");
+        if (activeCategory === "BMWM") {
+            carItems.forEach(car => {
+                if (car.dataset.type === "M") {
+                    car.style.display = "block";
+                } else {
+                    car.style.display = "none";
+                    car.style.marginLeft = "0";
+                }
+            });
+        }
+        else if (activeCategory === "BMWi") {
+            carItems.forEach(car => {
+                if (car.dataset.type === "i") {
+                    car.style.marginLeft = "2.5rem";
+                    car.style.display = "block";
+                } else {
 
-        carItems.forEach(car => {
-            const carCategory = car.dataset.category;
-            const carType = car.dataset.type;
+                    car.style.display = "none";
+                }
+            });
+        }
+        else {
+            carItems.forEach(car => {
+                const carCategory = car.dataset.category;
+                const carType = car.dataset.type;
 
-            if (activeCategory === "BMWM" && carType === "M") {
-                car.style.display = "block";
-                car.style.marginLeft = "0";
-            } else if (activeCategory === "BMWi" && carType === "i") {
-                car.style.display = "block";
-                car.style.marginLeft = "2.5rem";
-            } else if (activeCategory === carCategory && (!activeType || carType === activeType)) {
-                car.style.display = "block";
-                car.style.marginLeft = "2.5rem";
-            } else {
-                car.style.display = "none";
-            }
-        });
+                if (carCategory === activeCategory && carType === activeType) {
+                    car.style.marginLeft = "2.5rem";
+                    car.style.display = "block";
+                } else {
+                    car.style.display = "none";
+                }
+            });
+        }
+
     }
 
-    tabButtons.forEach(button => {
-        button.addEventListener("click", function () {
+    tabButtons.forEach(a => {
+        a.addEventListener("click", function () {
             tabButtons.forEach(btn => btn.classList.remove("active"));
             this.classList.add("active");
-            filterSubTabs(this.dataset.category);
+            filterSubTabs(this.dataset.category)
             filterCars();
         });
     });
-
-    subTabButtons.forEach(button => {
-        button.addEventListener("click", function () {
+    subTabButtons.forEach(a => {
+        a.addEventListener("click", function () {
             subTabButtons.forEach(btn => btn.classList.remove("sub-active"));
             this.classList.add("sub-active");
             filterCars();
